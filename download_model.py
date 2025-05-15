da4068668fd15f46590f14a09bffd00a670f2942
Marious Senesh <mariousenesh@gmail.com>
2025-05-15 17:03:11 +0530

    Switch to Hugging Face hosted model. Removed large file from repo

---
+++ download_model.py
@@ -0,0 +1,13 @@
+# download_model.py
+import requests
+import os
+
+url = "https://huggingface.co/ICESCOTTEA/banking-assistant-models/upload/main/transformer_intent_model.pkl"
+output_path = "models/transformer_intent_model.pkl"
+
+os.makedirs("models", exist_ok=True)
+print("Downloading model...")
+r = requests.get(url)
+with open(output_path, "wb") as f:
+    f.write(r.content)
+print("Download complete.")
